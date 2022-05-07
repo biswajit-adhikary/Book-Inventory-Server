63c497a81bc9e49fe3cf91ab15a781e14bf7b16b
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-07 17:36:26 +0600

    Delete API added

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ async function run() {
             res.send(result);
         })
 
+        // Create delete API
+        app.delete('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookCollection.deleteOne(query);
+            res.send(result);
+        })
+
     } finally { }
 }
 run().catch(console.dir);
