7e9bf164f2ce9fc4549db81758a1a56a8264eb74
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-07 14:00:05 +0600

    Create API for one book using ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 
@@ -27,6 +27,14 @@ async function run() {
             const cursor = bookCollection.find(query);
             const books = await cursor.toArray();
             res.send(books);
+        });
+
+        // Create API for get one book using ID
+        app.get('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const book = await bookCollection.findOne(query);
+            res.send(book);
         });
 
     } finally { }
